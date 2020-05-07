fd001e8c3a8b7bee2b44386432654afb3092664e
devjwadhwa <55413095+devjwadhwa@users.noreply.github.com>
2020-05-06 20:24:14 -0500

    New

--- proj/cmake/ciAnimatedGifConfig.cmake
+++ proj/cmake/ciAnimatedGifConfig.cmake
@@ -1,37 +1,42 @@
 if(NOT TARGET ciAnimatedGif)
-    # Define ${ciAnimatedGif_PROJECT_ROOT}. ${CMAKE_CURRENT_LIST_DIR} is just the current directory.
+    # Define ${Cinder-MIDI2_PROJECT_ROOT}. ${CMAKE_CURRENT_LIST_DIR} is just the current directory.
     get_filename_component(ciAnimatedGif_PROJECT_ROOT "${CMAKE_CURRENT_LIST_DIR}/../.." ABSOLUTE)
 
     # Define ${CINDER_PATH} as usual.
     get_filename_component(CINDER_PATH "${ciAnimatedGif_PROJECT_ROOT}/../.." ABSOLUTE)
 
-    # Make a list of source files and define that to be ${SOURCE_LIST}.
+    #    # Translate the <staticLibrary> tag.
+    #    # pocketsphinx
+    #    add_library(pocketsphinx STATIC IMPORTED)
+    #    set_property(TARGET pocketsphinx PROPERTY IMPORTED_LOCATION "${Cinder-MIDI2_PROJECT_ROOT}/lib/macosx/libpocketsphinx.a")
+    #
+    #    # sphinxbase
+    #    add_library(sphinxbase STATIC IMPORTED)
+    #    set_property(TARGET sphinxbase PROPERTY IMPORTED_LOCATION "${Cinder-MIDI2_PROJECT_ROOT}/lib/macosx/libsphinxbase.a")
+
+    # Translate the <sourcePattern> tag.
     file(GLOB SOURCE_LIST CONFIGURE_DEPENDS
-            "${ciAnimatedGif_PROJECT_ROOT}/src/rph/ciAnimatedGif.cpp"
-            "${ciAnimatedGif_PROJECT_ROOT}/src/rph/ciAnimatedGif.h"
+            "${ciAnimatedGif_PROJECT_ROOT}/src/*.cpp"
             )
 
-    # Create the library!
+    # Create the library from the source files. The target is now defined.
     add_library(ciAnimatedGif ${SOURCE_LIST})
 
-    # Add include directories.
-    # Notice that `cinderblock.xml` has `<includePath>src</includePath>`.
-    # So you need to set `../../src/` to include.
-    target_include_directories(ciAnimatedGif PUBLIC "${ciAnimatedGif_PROJECT_ROOT}/src" )
+    # Link the prebuilt libraries.
+    #target_link_libraries(-framework CoreMIDI)
+    #    target_link_libraries(Cinder-MIDI2 CoreMIDI.framework)
+    #    target_link_libraries(Cinder-MIDI2 -framework CoreMIDI.framework)
+
+    # Translate <includePath> tag.
+    target_include_directories(ciAnimatedGif PUBLIC
+            "${ciAnimatedGif_PROJECT_ROOT}/src/"
+            )
+
+    # Translate <headerPattern> tag.
+    target_include_directories(ciAnimatedGif PRIVATE
+            #            "${Cinder-MIDI2_PROJECT_ROOT}/include/sphinx"
+            "${ciAnimatedGif_PROJECT_ROOT}/src/*.h"
+            )
+
     target_include_directories(ciAnimatedGif SYSTEM BEFORE PUBLIC "${CINDER_PATH}/include" )
-
-
-    # If your Cinder block has no source code but instead pre-build libraries,
-    # you can specify all of them here (uncomment the below line and adjust to your needs).
-    # Make sure to use the libraries for the right platform.
-    # # target_link_libraries(ciAnimatedGif "${Cinder-OpenCV_PROJECT_ROOT}/lib/libopencv_core.a")
-
-    if(NOT TARGET cinder)
-        include("${CINDER_PATH}/proj/cmake/configure.cmake")
-        find_package(cinder REQUIRED PATHS
-                "${CINDER_PATH}/${CINDER_LIB_DIRECTORY}"
-                "$ENV{CINDER_PATH}/${CINDER_LIB_DIRECTORY}")
-    endif()
-    target_link_libraries(ciAnimatedGif PRIVATE cinder)
-
 endif()
